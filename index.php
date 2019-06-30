477fd50c6718bf16f44129c95dec270b3e7cdaed
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-06-29 17:47:36 -0700

    step 3.5

--- index.php
+++ index.php
@@ -31,13 +31,38 @@ in html
 			<a href="about.php">About</a>
 			<a href="contact.php">Contact</a>
 		</nav>
-        
-        
+		
+		<br /><br />
+		
+		<main>
+			
+			<figure>
+				<img src="img/anthony_davis.png" alt="Picture of Anthony Davis" />
+			</figure>
+			
+			<div>
+				Hello! <br />
+				<p>Thank you for visiting my professional portfolio website.</p>
+				
+				<p>I am a Software Engineer and have been recently hired at the Acme Corporation.</p>
+				
+				<p><a href="contact.php">Feel free to contact me!</a></p>
+				
+				<br /><br />
+				
+				<em>"With ordinary talent and extraordinary <strong>perseverance</strong>, all things are attainable"</em><br />
+				- Thomas F. Buxton
+			</div>
+			
+		</main>
+		
         <!-- This is the footer -->
         <!-- The footer goes inside the body but not always -->
         <footer>
-            
-            
+			<hr>
+				CST336 Internet Programming. 2019&copy; Davis <br />
+				<strong>Disclaimer:</strong>The information in this webpage is fictitious.<br />
+				It is used for academic purposes only.  
         </footer>
         <!-- closing footer -->
         
